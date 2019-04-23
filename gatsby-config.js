cf009a6964cfaafcf65b5e14875bcf895903d3ef
Carlo Schneider <contact@carloschneider.dev>
2019-04-23 22:14:38 +0000

    feat: added dependencies to handle images

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+  ]
+}
